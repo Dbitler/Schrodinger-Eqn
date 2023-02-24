0e217dcfa522158114b7b711242a9c922cad6958
Dbitler <dbitler@hawk.iit.edu>
2023-02-24 15:02:41 -0600

    Initial Commit

---
+++ Schrodinger Eqn/Schrodinger_EqnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schrodinger_EqnApp.swift
+//  Schrodinger Eqn
+//
+//  Created by IIT PHYS 440 on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Schrodinger_EqnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
